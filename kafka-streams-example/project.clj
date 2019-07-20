1e11a7a0df5e3178865c5ccd4e8e0db7d177e2c8
perkss <stuart.m.perks@gmail.com>
2019-07-20 10:57:26 +0100

    Update dependencies

--- kafka-streams-example/project.clj
+++ kafka-streams-example/project.clj
@@ -3,15 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.json "0.2.6"]
-                 [org.apache.kafka/kafka-streams "2.0.0"]
-                 [org.apache.kafka/kafka-clients "2.0.0"]
-                 [org.apache.kafka/kafka-streams-test-utils "2.0.0"]
-                 [org.apache.kafka/kafka-clients "1.1.0" :classifier "test"]
-                 [org.apache.kafka/connect-json "2.0.0"]
+                 [org.apache.kafka/kafka-streams "2.2.0"]
+                 [org.apache.kafka/kafka-clients "2.2.0"]
+                 [org.apache.kafka/kafka-streams-test-utils "2.3.0"]
+                 [org.apache.kafka/kafka-clients "2.3.0" :classifier "test"]
                  [io.confluent/kafka-streams-avro-serde "5.0.0"]
-                 [org.springframework.kafka/spring-kafka "1.2.2.RELEASE"]
                  [org.clojure/tools.logging "0.4.0"]
                  [org.slf4j/slf4j-log4j12 "1.7.1"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
