d7195512f5682de046dddbe19b1cb1ff0791c404
perkss <stuart.m.perks@gmail.com>
2019-07-20 18:32:59 +0100

    Updates to starting of producer and consumer

--- kafka-producer-consumer-example/start.sh
+++ kafka-producer-consumer-example/start.sh
@@ -2,12 +2,14 @@
 # Start Zookeeper
 /usr/local/zookeeper/bin/zkServer.sh start
 # Start Kafka
-/usr/local/kafka/bin/kafka-server-start.sh -daemon /usr/local/kafka/config/server.properties
-# Create topic topic that the application consums from 
-/usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic example-topic
+/usr/local/bin/kafka/bin/kafka-server-start.sh -daemon /usr/local/kafka/config/server.properties
+# Create topic topic that the application consumes from
+/usr/local/bin/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic example-consumer-topic
 # Create the consuming topic from the example that produces to it
-/usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic example-produced-topic
+/usr/local/bin/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic example-produced-topic
+# List the created
+/usr/local/bin/kafka/bin/kafka-topics.sh --list --zookeeper localhost:2181
 # Produce messages for the app to consumer
-/usr/local/kafka/bin/kafka-console-producer.shroker-list localhost:9092 --topic example-topic
+/usr/local/bin/kafka/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic example-consumer-topic
 # Consume the output messages
-/usr/local/kafka/bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic example-produced-topic --from-beginning
+/usr/local/bin/kafka/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic example-produced-topic --from-beginning
--- kafka-producer-consumer-example/test/kafka_example/core_test.clj
+++ kafka-producer-consumer-example/test/kafka_example/core_test.clj
@@ -1,7 +1,3 @@
 (ns kafka-example.core-test
   (:require [clojure.test :refer :all]
             [kafka-example.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
