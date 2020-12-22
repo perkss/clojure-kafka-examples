ff9a9baaada281cef7c1832fbd651480badacb91
perkss <stuart.m.perks@gmail.com>
2020-12-22 22:29:21 +0000

    Upgrade Kafka to 2.7.0
    Update travis.yml to run docker tests.

--- kafka-producer-consumer-example/project.clj
+++ kafka-producer-consumer-example/project.clj
@@ -3,16 +3,16 @@
   :url "https://perkss.github.io/#/clojure/KafkaClojure#text-body"
   :dependencies [[environ "1.1.0"]
                  [org.clojure/clojure "1.10.1"]
-                 [org.apache.kafka/kafka-clients "2.4.1"]
-                 [org.apache.kafka/kafka_2.12 "2.4.1"]
+                 [org.apache.kafka/kafka-clients "2.7.0"]
+                 [org.apache.kafka/kafka_2.12 "2.7.0"]
                  [org.clojure/tools.logging "0.4.0"]
                  [org.slf4j/slf4j-log4j12 "1.7.1"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
                                                     javax.jms/jms
                                                     com.sun.jmdk/jmxtools
                                                     com.sun.jmx/jmxri]]
-                 [org.testcontainers/testcontainers "1.13.0"]
-                 [org.testcontainers/kafka "1.13.0"]]
+                 [org.testcontainers/testcontainers "1.15.1"]
+                 [org.testcontainers/kafka "1.15.1"]]
   :main ^:skip-aot kafka-example.core
   :target-path "target/%s"
   :plugins [[lein-cljfmt "0.6.0"]]
