a560252226498732d5ee894b4562afb180c8c86b
perkss <stuart.m.perks@gmail.com>
2020-12-22 22:29:21 +0000

    Upgrade Kafka to 2.7.0
    Update travis.yml to run docker tests.

--- kafka-producer-consumer-example/test/kafka_example/integration/example_integration_test.clj
+++ kafka-producer-consumer-example/test/kafka_example/integration/example_integration_test.clj
@@ -2,18 +2,20 @@
   (:require [clojure.test :refer :all]
             [kafka-example.core :refer :all])
   (:import (org.testcontainers.containers KafkaContainer)
-           (org.apache.kafka.clients.producer ProducerRecord)))
+           (org.apache.kafka.clients.producer ProducerRecord)
+           (org.testcontainers.utility DockerImageName)))
 
 (deftest example-kafka-integration-test
   (testing "Fire up test containers Kafka and then send and consume message"
     (let
-      [kafka-container (KafkaContainer. "5.4.1")
+      [kafka-container (KafkaContainer. (DockerImageName/parse "confluentinc/cp-kafka:5.5.3"))
        _ (.start kafka-container)
        bootstrap-server (.getBootstrapServers kafka-container)
        test-producer (build-producer bootstrap-server)
        _ (future (run-application bootstrap-server))        ; execute application in separate thread
        producer-topic "example-consumer-topic"
-       test-consumer (build-consumer "example-produced-topic" bootstrap-server)
+       test-consumer (build-consumer bootstrap-server)
+       _ (consumer-subscribe test-consumer "example-produced-topic")
        input-data "hello"
        sent-result (.get (.send test-producer (ProducerRecord. producer-topic input-data)))
        records (.poll test-consumer 10000)]
