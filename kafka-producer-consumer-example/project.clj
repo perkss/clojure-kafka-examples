5746ec8fccbae7c6d650b29517c6d17291c95273
perkss <stuart.m.perks@gmail.com>
2023-05-09 19:39:05 +0100

    Update to kafka 3.4.0

--- kafka-producer-consumer-example/project.clj
+++ kafka-producer-consumer-example/project.clj
@@ -2,15 +2,12 @@
   :description "Example Kafka Producer and Consumer using Plain Java Interop"
   :url "https://perkss.github.io/#/clojure/KafkaClojure#text-body"
   :dependencies [[environ "1.1.0"]
-                 [org.clojure/clojure "1.10.1"]
-                 [org.apache.kafka/kafka-clients "3.0.0"]
-                 [org.apache.kafka/kafka_2.12 "3.0.0"]
-                 [org.clojure/tools.logging "0.4.0"]
-                 [org.slf4j/slf4j-log4j12 "1.7.1"]
-                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
-                                                    javax.jms/jms
-                                                    com.sun.jmdk/jmxtools
-                                                    com.sun.jmx/jmxri]]
+                 [org.clojure/clojure "1.11.1"]
+                 [org.apache.kafka/kafka-clients "3.4.0"]
+                 [org.apache.kafka/kafka_2.12 "3.4.0"]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [org.slf4j/slf4j-log4j12 "2.0.7"]
+                 [org.apache.logging.log4j/log4j-core "2.20.0"]
                  [org.testcontainers/testcontainers "1.15.3"]
                  [org.testcontainers/kafka "1.15.3"]]
   :main ^:skip-aot kafka-example.core
