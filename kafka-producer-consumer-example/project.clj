cd5cb20cfe53877892b74ac8fc07b7ea706d4dea
perkss <stuart.m.perks@gmail.com>
2023-12-30 20:05:52 +0000

    Upgrade to kafka 3.6.1

--- kafka-producer-consumer-example/project.clj
+++ kafka-producer-consumer-example/project.clj
@@ -3,13 +3,13 @@
   :url "https://perkss.github.io/#/clojure/KafkaClojure#text-body"
   :dependencies [[environ "1.1.0"]
                  [org.clojure/clojure "1.11.1"]
-                 [org.apache.kafka/kafka-clients "3.4.0"]
-                 [org.apache.kafka/kafka_2.12 "3.4.0"]
+                 [org.apache.kafka/kafka-clients "3.6.1"]
+                 [org.apache.kafka/kafka_2.12 "3.6.1"]
                  [org.clojure/tools.logging "1.2.4"]
-                 [org.slf4j/slf4j-log4j12 "2.0.7"]
-                 [org.apache.logging.log4j/log4j-core "2.20.0"]
-                 [org.testcontainers/testcontainers "1.15.3"]
-                 [org.testcontainers/kafka "1.15.3"]]
+                 [org.slf4j/slf4j-log4j12 "2.0.10"]
+                 [org.apache.logging.log4j/log4j-core "2.22.1"]
+                 [org.testcontainers/testcontainers "1.19.3"]
+                 [org.testcontainers/kafka "1.19.3"]]
   :main ^:skip-aot kafka-example.core
   :target-path "target/%s"
   :plugins [[lein-cljfmt "0.6.0"]]
